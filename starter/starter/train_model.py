b12026787f6b85016bb953d74e71f0845a4209c1
Samuel Amico <47945745+samuelamico@users.noreply.github.com>
2021-12-18 15:21:22 -0300

    add slicing function

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -6,7 +6,7 @@ from ml.model import train_model, compute_model_metrics, inference
 import logging
 import pickle
 from sklearn.metrics import mean_absolute_error
-
+import os
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
@@ -71,3 +71,36 @@ logger.info("Metrcis for RF model")
 r_squared = randomforest.score(X_test, y_test)
 mae = mean_absolute_error(y_test, y_test_pred_gb)
 logger.info(f"R_squared = {r_squared} --- MAE = {mae}")
+
+# performance testing using model slicing
+data_dir = os.getcwd()
+
+data = pd.read_csv("../data/census_cleaned.csv")
+
+gb_model = os.path.join(data_dir, "../model/gbclassifier.pkl")
+with open(gb_model, "rb") as f:
+    gb_model = pickle.load(f)
+
+rf_model = os.path.join(data_dir, "../model/randomforest.pkl")
+with open(rf_model, "rb") as f:
+    rf_model = pickle.load(f)
+
+encoder = os.path.join(data_dir, "../model/encoder.pkl")
+with open(encoder, "rb") as f:
+    encoder = pickle.load(f)
+
+lb = os.path.join(data_dir, "../model/lb.pkl")
+with open(lb, "rb") as f:
+    lb = pickle.load(f)
+
+dataframe = pd.DataFrame(columns=["feature", "value", "precision", "recall", "fbeta_score"])
+for feature in cat_features:
+    for value in data[feature].unique():
+        subset_data = data[data[feature] == value]
+        X_test_subset, y_test_subset, encoder, lb = process_data(
+            subset_data, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+        )
+        y_pred_subset = gb_model.predict(X_test_subset)
+        precision, recall, fbeta = compute_model_metrics(y_test_subset, y_pred_subset)
+        dataframe = dataframe.append({"feature": feature, "value":value, "precision": precision, "recall": recall, "fbeta_score": fbeta}, ignore_index = True)
+dataframe.to_csv('slicing_columns_test.csv',index=False)
